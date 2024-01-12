79adf95c66d4c011822944fb59dd5b1cbd6862e5
TATA KRZYSIO <TATA KRZYSIO@DESKTOP-1RV2VH9>
2024-01-12 19:42:28 +0100

    Add project files.

---
+++ Heloworld/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Heloworld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("nazywam się krzysztof Janiak");
+        }
+
+    }
+}
